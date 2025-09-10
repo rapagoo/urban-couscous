988df8a06700b6fcae8a7406f903f136ac3608a2
sungrok <lilspicypepper@gmail.com>
2025-09-10 14:42:49 +0900

    Window 띄워보기

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::Window window(sf::VideoMode({ 800, 600 }), "My Window");
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is < sf::Event::Closed>())
+				window.close();
+		}
+	}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 }
