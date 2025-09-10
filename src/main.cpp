210d932bb9f531c31fd398cdf6b990f30afcb0c3
sungrok <lilspicypepper@gmail.com>
2025-09-10 16:11:47 +0900

    SFML 그래픽 기능 사용을 위한 코드 수정
    헤더 파일을 `<SFML/Graphics.hpp>`로 변경하고,
    `sf::Window`를 `sf::RenderWindow`로 수정하여
    그래픽 렌더링을 지원합니다. 이벤트 처리 루프에
    `const std::optional event`를 추가하고,
    이벤트 확인 구문에서 불필요한 공백을 제거했습니다.
    마지막으로, 매 프레임마다 창을 지우고
    새로 그린 내용을 표시하기 위해 `window.clear()`와
    `window.display()`를 추가했습니다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode({ 800, 600 }), "My Window");
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My Window");
 
 	while (window.isOpen())
 	{
 		while (const std::optional event = window.pollEvent())
 		{
-			if (event->is < sf::Event::Closed>())
+			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
+		window.clear();
+		window.display();
 	}
-
 }
